1dadd7153402e0915ff2091604b599c8f46531d6
ShaikVasid <98427286+ShaikVasid@users.noreply.github.com>
2026-08-22 21:21:07 -0400

    feat: add Azure networking outputs

--- modules/networking/outputs.tf
+++ modules/networking/outputs.tf
@@ -1,3 +1,5 @@
-output "vpc_id" { value = aws_vpc.this.id }
-output "public_subnet_ids" { value = aws_subnet.public[*].id }
-output "private_subnet_ids" { value = aws_subnet.private[*].id }
+output "resource_group_name" { value = azurerm_resource_group.this.name }
+output "resource_group_id" { value = azurerm_resource_group.this.id }
+output "vnet_id" { value = azurerm_virtual_network.this.id }
+output "public_subnet_id" { value = azurerm_subnet.public.id }
+output "private_subnet_id" { value = azurerm_subnet.private.id }
