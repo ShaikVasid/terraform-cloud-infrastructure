9906f0852a11ebfcdac80cf975a963b38f87097b
ShaikVasid <98427286+ShaikVasid@users.noreply.github.com>
2026-08-22 22:10:54 -0400

    refactor: centralize dev environment variables

--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -1,11 +1,29 @@
 variable "location" {
-  description = "Azure region for the environment"
+  description = "Azure region where the environment is deployed."
   type        = string
-  default     = "Canada Central"
+  default     = "canadacentral"
 }
 
 variable "resource_group_name" {
-  description = "Resource group for the environment"
+  description = "Resource group name for the environment."
   type        = string
   default     = "rg-portfolio-dev"
+}
+
+variable "address_space" {
+  description = "CIDR address space for the Azure virtual network."
+  type        = string
+  default     = "10.20.0.0/16"
+}
+
+variable "public_subnet_prefix" {
+  description = "CIDR prefix for the public subnet."
+  type        = string
+  default     = "10.20.1.0/24"
+}
+
+variable "private_subnet_prefix" {
+  description = "CIDR prefix for the private subnet."
+  type        = string
+  default     = "10.20.11.0/24"
 }
