a5e5e9eaa2995b28577655a563a7a3ace8afbdec
ShaikVasid <98427286+ShaikVasid@users.noreply.github.com>
2026-08-22 21:21:25 -0400

    refactor: replace AWS IAM with Azure managed identity and RBAC

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -1,20 +1,16 @@
 variable "name" { type = string }
+variable "resource_group_id" { type = string }
 variable "tags" { type = map(string) default = {} }
 
-resource "aws_iam_role" "ec2" {
-  name = "${var.name}-ec2-role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Effect = "Allow",
-      Principal = { Service = "ec2.amazonaws.com" },
-      Action = "sts:AssumeRole"
-    }]
-  })
-  tags = var.tags
+resource "azurerm_user_assigned_identity" "workload" {
+  name                = "${var.name}-identity"
+  location            = "Canada Central"
+  resource_group_name = element(split("/", var.resource_group_id), 4)
+  tags                = var.tags
 }
 
-resource "aws_iam_instance_profile" "ec2" {
-  name = "${var.name}-ec2-profile"
-  role = aws_iam_role.ec2.name
+resource "azurerm_role_assignment" "reader" {
+  scope                = var.resource_group_id
+  role_definition_name = "Reader"
+  principal_id         = azurerm_user_assigned_identity.workload.principal_id
 }
