0fb2d8cc6decdeb40a929211efb3d5caee577c1a
ShaikVasid <98427286+ShaikVasid@users.noreply.github.com>
2026-08-22 22:10:20 -0400

    refactor: separate identity module variables

--- modules/iam/variables.tf
+++ modules/iam/variables.tf
@@ -1,2 +1,25 @@
-variable "name" { type = string }
-variable "tags" { type = map(string) default = {} }
+variable "name" {
+  description = "Base name used for identity resources."
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region where the managed identity is deployed."
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "Resource group containing the managed identity."
+  type        = string
+}
+
+variable "resource_group_id" {
+  description = "Resource ID of the resource group receiving the RBAC assignment."
+  type        = string
+}
+
+variable "tags" {
+  description = "Tags applied to identity resources."
+  type        = map(string)
+  default     = {}
+}
