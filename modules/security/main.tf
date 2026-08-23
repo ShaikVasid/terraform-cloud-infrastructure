51c9319c79c32184b1151e3c7900a2848a47de26
ShaikVasid <98427286+ShaikVasid@users.noreply.github.com>
2026-08-22 22:10:15 -0400

    refactor: clean Azure security module

--- modules/security/main.tf
+++ modules/security/main.tf
@@ -1,8 +1,3 @@
-variable "name" { type = string }
-variable "resource_group_name" { type = string }
-variable "location" { type = string }
-variable "tags" { type = map(string) default = {} }
-
 resource "azurerm_network_security_group" "application" {
   name                = "${var.name}-nsg"
   location            = var.location
