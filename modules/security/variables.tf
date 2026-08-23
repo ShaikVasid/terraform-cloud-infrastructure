36ef8976a0dc2f08493f68c94b46e7e40f9fa9b0
ShaikVasid <98427286+ShaikVasid@users.noreply.github.com>
2026-08-22 22:10:11 -0400

    refactor: separate security variables

--- modules/security/variables.tf
+++ modules/security/variables.tf
@@ -1,3 +1,20 @@
-variable "name" { type = string }
-variable "vpc_id" { type = string }
-variable "tags" { type = map(string) default = {} }
+variable "name" {
+  description = "Base name used for security resources."
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "Resource group containing the security resources."
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region where security resources are deployed."
+  type        = string
+}
+
+variable "tags" {
+  description = "Tags applied to security resources."
+  type        = map(string)
+  default     = {}
+}
