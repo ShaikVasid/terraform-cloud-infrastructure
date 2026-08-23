c05122a40629f420a8fd664a5726180bd936dc69
ShaikVasid <98427286+ShaikVasid@users.noreply.github.com>
2026-08-22 22:10:50 -0400

    refactor: format identity module outputs

--- modules/iam/outputs.tf
+++ modules/iam/outputs.tf
@@ -1,3 +1,14 @@
-output "identity_id" { value = azurerm_user_assigned_identity.workload.id }
-output "principal_id" { value = azurerm_user_assigned_identity.workload.principal_id }
-output "client_id" { value = azurerm_user_assigned_identity.workload.client_id }
+output "identity_id" {
+  description = "Resource ID of the workload managed identity."
+  value       = azurerm_user_assigned_identity.workload.id
+}
+
+output "principal_id" {
+  description = "Principal ID of the workload managed identity."
+  value       = azurerm_user_assigned_identity.workload.principal_id
+}
+
+output "client_id" {
+  description = "Client ID of the workload managed identity."
+  value       = azurerm_user_assigned_identity.workload.client_id
+}
