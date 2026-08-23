8241ec83612c367091babf1352fc679686535718
ShaikVasid <98427286+ShaikVasid@users.noreply.github.com>
2026-08-22 21:21:13 -0400

    refactor: replace AWS security group with Azure NSG

--- modules/security/main.tf
+++ modules/security/main.tf
@@ -1,26 +1,36 @@
 variable "name" { type = string }
-variable "vpc_id" { type = string }
+variable "resource_group_name" { type = string }
+variable "location" { type = string }
 variable "tags" { type = map(string) default = {} }
 
-resource "aws_security_group" "application" {
-  name        = "${var.name}-application"
-  description = "Application security group"
-  vpc_id      = var.vpc_id
+resource "azurerm_network_security_group" "application" {
+  name                = "${var.name}-nsg"
+  location            = var.location
+  resource_group_name = var.resource_group_name
 
-  ingress {
-    description = "HTTP from trusted network"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/16"]
+  security_rule {
+    name                       = "allow-http-from-vnet"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "VirtualNetwork"
+    destination_address_prefix = "*"
   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+  security_rule {
+    name                       = "deny-internet-inbound"
+    priority                   = 4000
+    direction                  = "Inbound"
+    access                     = "Deny"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "Internet"
+    destination_address_prefix = "*"
   }
 
-  tags = merge(var.tags, { Name = "${var.name}-application-sg" })
+  tags = var.tags
 }
